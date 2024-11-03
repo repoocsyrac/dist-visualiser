a299ff7248d974fe5586ae0597d22d2d634052ce
syrac <carys.cooper@univ.ox.ac.uk>
2024-11-03 00:36:07 +0000

    Add exponential dist parameter selection

--- main.py
+++ main.py
@@ -20,8 +20,13 @@ if dist_choice == "Normal":
     st.write(f"### Normal distribution with μ={mean} and σ={std_dev}")
 elif dist_choice == "Poisson":
     st.write("## Poisson Distribution Parameters")
-    mean = st.sidebar.slider("Lambda()", 1, 20, 5)
-    x = np.arrange(0, 30)
+    mean = st.sidebar.slider("Lambda(λ)", 1, 20, 5)
+    x = np.arange(0, 30)
     y = poisson.pmf(x, mean)
-    st.write(f"### Poisson distribution with ={mean}")
-    
+    st.write(f"### Poisson distribution with λ={mean}")
+elif dist_choice == "Exponential":
+    st.sidebar.write("## Exponential Distribution Parameters")
+    rate = st.sidebar.slider("Rate (λ)", 0.1, 2.0, 1.0)
+    x = np.linspace(0, 10, 1000)
+    y = expon.pdf(x, scale=1/rate)
+    st.write(f"### Exponential distribution with λ={rate}")
